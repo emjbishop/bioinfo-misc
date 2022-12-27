ccb5a71120a8b09fa6b726301dc60453bfa6325c
Emma Bishop <bishemma1@gmail.com>
2022-12-27 14:45:47 -0800

    Count duplicate genes and write out new fasta

--- multifasta_to_gtf.py
+++ multifasta_to_gtf.py
@@ -1,28 +1,58 @@
 from Bio import SeqIO
+from collections import defaultdict
+import argparse
 
 '''
-Create generic GTF records from a multifasta, with the intention
-of them being appended to a reference GTF. Inspiried by cellranger's "Add a 
+Create generic GTF records and new fasta from a fasta/multifasta, with the intention
+of them being appended to a reference. Inspiried by cellranger's "Add a
 marker gene to the FASTA and GTF" docs:
 
 https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/using/tutorial_mr#marker
 '''
 
-in_multifasta = "path/to/in.fasta"  # Input fasta or multifasta
-out_gtf = "path/to/out.gtf" # Desired output file
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input",
+                    help="Input fasta or multifasta")
+parser.add_argument("-g", "--gtf",
+                    help="Output GTF")
+parser.add_argument("-f", "--fasta",
+                    help="Output fasta/multifasta")
+args = parser.parse_args()
 
-with open(in_multifasta) as f:
+
+with open(args.input) as f:
+
+    # Will use this to count duplicate gene names. Default value is 0.
+    seen = defaultdict(int)
+
 
     for fasta_record in SeqIO.parse(f, "fasta"):
-        gene = fasta_record.id  # My fasta ids are just the gene names
-        length = len(fasta_record.seq)
+        # Parse fasta/multifasta (my ids are just the gene names)
+        seq = fasta_record.seq
+        length = len(seq)
+        id = fasta_record.id
 
-        # GTF record
-        outline = '{gene}\tunknown\texon\t1\t{length}\t.\t+\t.\tgene_id "{gene}"; ' \
-               'transcript_id "{gene}"; gene_name "{gene}"; ' \
-               'gene_biotype "protein_coding";\n'.format(gene = gene, 
-                                                         length = length)
+        # Add gene to dictionary (if not in there already) and increase counter
+        seen[id] +=1
+
+        # If there are multiples of this gene, append ".2" etc to name (e.g. TRAV5.2)
+        if seen[id] > 1:
+            gene = id + '.' + str(seen[id])
+        else:
+            gene = id
+
+        # Generate gtf record
+        gtf_record = '{gene}\tunknown\texon\t1\t{length}\t.\t+\t.\t' \
+                     'gene_id "{gene}"; transcript_id "{gene}"; ' \
+                     'gene_name "{gene}"; ' \
+                     'gene_biotype "protein_coding";\n'.format(gene = gene,
+                                                               length = length)
         
-        # Append to output GTF
-        with open(out_gtf, 'a') as outfile:
-            outfile.write(outline)
+        # Append to output gtf
+        with open(args.gtf, 'a') as g:
+            g.write(gtf_record)
+
+        # Output new multifasta with updated (e.g. TRAV5.2) gene names
+        with open(args.fasta, 'a') as f:
+            f.write('>' + gene + '\n')
+            f.write(str(seq) + '\n')
